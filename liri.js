3c9f60f5d87eb81d35deee8b1b726bd7ba58f603
James Roth <jkltroth@gmail.com>
2017-11-26 23:29:51 -0500

    Spotify-this-song functionality complete. Movie-this functionality in progress.

--- liri.js
+++ liri.js
@@ -30,31 +30,76 @@ if (command === 'my-tweets') {
 
 } else if (command === 'spotify-this-song') {
 
+    let songName = process.argv[3];
     let spotifyClient = new Spotify(spotifyKeys);
-    let songName = process.argv[3].toLowerCase();
 
-    spotifyClient.search({
-        type: 'track',
-        query: songName,
-        limit: 1,
-    }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
+    if (!songName) {
 
-        let trackData = data.tracks.items[0];
+        console.log("Not a valid song.");
 
-        console.log(trackData);
-        console.log("-----------------------");
-        console.log("Artist(s): " + trackData.artists[0].name);
-        console.log("Title: " + trackData.name);
-        console.log("Preview Link: " + trackData.preview_url);
-        console.log("Album: " + trackData.album.name);
-    });
+        spotifyClient
+            .request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
+            .then(function (data) {
+
+                console.log(data);
+                console.log("-----------------------");
+                console.log("Artist(s): " + data.artists[0].name);
+                console.log("Title: " + data.name);
+                console.log("Preview Link: " + data.preview_url);
+                console.log("Album: " + data.album.name);
+
+            })
+            .catch(function (err) {
+                console.error('Error occurred: ' + err);
+            });
+
+    } else if (songName) {
+
+        spotifyClient.search({
+            type: 'track',
+            query: songName,
+            limit: 1,
+        }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+
+            let trackData = data.tracks.items[0];
+
+            console.log(trackData);
+            console.log("-----------------------");
+            console.log("Artist(s): " + trackData.artists[0].name);
+            console.log("Title: " + trackData.name);
+            console.log("Preview Link: " + trackData.preview_url);
+            console.log("Album: " + trackData.album.name);
+        });
+    }
 
 } else if (command === 'movie-this') {
 
-    // movie-this logic
+    let movieName = process.argv[3];
+
+    // Then run a request to the OMDB API with the movie specified
+    request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece", function (error, response, body) {
+
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
+
+            let movieData = JSON.parse(body)
+
+            // Parse the body of the site and recover just the imdbRating
+            console.log(movieData);
+            console.log("-----------------------");
+            console.log("Title: " + movieData.Title);
+            console.log("Year: " + movieData.Year);
+            console.log("IMDB Rating: " + "");
+            console.log("Rotten Tomatoes Rating: " + "");
+            console.log("Country: " + movieData.Country);
+            console.log("Language: " + movieData.Language);
+            console.log("Plot: " + movieData.Plot);
+            console.log("Actors: " + movieData.Actors);
+        }
+    });
 
 } else if (command === 'do-what-it-says') {
 
