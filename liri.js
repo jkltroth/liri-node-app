4728fe98c4dbe9c37ba50c1d894f48d6d63e3f0e
James Roth <jkltroth@gmail.com>
2017-11-27 22:12:20 -0500

    MVP. Need to refactor if statements into functions, and remove unnecessary console.log info

--- liri.js
+++ liri.js
@@ -9,7 +9,6 @@ const file = "random.txt";
 
 const command = process.argv[2].toLowerCase();
 
-
 if (command === 'my-tweets') {
 
     let twitterClient = new Twitter(twitterKeys);
@@ -35,13 +34,10 @@ if (command === 'my-tweets') {
 
     if (!songName) {
 
-        console.log("Not a valid song.");
-
         spotifyClient
             .request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
             .then(function (data) {
 
-                console.log(data);
                 console.log("-----------------------");
                 console.log("Artist(s): " + data.artists[0].name);
                 console.log("Title: " + data.name);
@@ -79,31 +75,87 @@ if (command === 'my-tweets') {
 
     let movieName = process.argv[3];
 
-    // Then run a request to the OMDB API with the movie specified
-    request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece", function (error, response, body) {
+    if (!movieName) {
 
-        // If the request is successful (i.e. if the response status code is 200)
-        if (!error && response.statusCode === 200) {
+        // Then run a request to the OMDB API with the movie specified
+        request("http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=40e9cece", function (error, response, body) {
 
-            let movieData = JSON.parse(body)
+            // If the request is successful (i.e. if the response status code is 200)
+            if (!error && response.statusCode === 200) {
 
-            // Parse the body of the site and recover just the imdbRating
-            console.log(movieData);
-            console.log("-----------------------");
-            console.log("Title: " + movieData.Title);
-            console.log("Year: " + movieData.Year);
-            console.log("IMDB Rating: " + "");
-            console.log("Rotten Tomatoes Rating: " + "");
-            console.log("Country: " + movieData.Country);
-            console.log("Language: " + movieData.Language);
-            console.log("Plot: " + movieData.Plot);
-            console.log("Actors: " + movieData.Actors);
-        }
-    });
+                let movieData = JSON.parse(body)
+
+                // Parse the body of the site and recover just the imdbRating
+                console.log("-----------------------");
+                console.log(movieData);
+                console.log("-----------------------");
+                console.log("Title: " + movieData.Title);
+                console.log("Year: " + movieData.Year);
+                console.log("IMDB Rating: " + movieData.Ratings[0].Value);
+                console.log("Rotten Tomatoes Rating: " + movieData.Ratings[1].Value);
+                console.log("Country: " + movieData.Country);
+                console.log("Language: " + movieData.Language);
+                console.log("Plot: " + movieData.Plot);
+                console.log("Actors: " + movieData.Actors);
+            }
+        });
+
+    } else if (movieName) {
+
+        // Then run a request to the OMDB API with the movie specified
+        request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece", function (error, response, body) {
+
+            // If the request is successful (i.e. if the response status code is 200)
+            if (!error && response.statusCode === 200) {
+
+                let movieData = JSON.parse(body)
+
+                // Parse the body of the site and recover just the imdbRating
+                console.log("-----------------------");
+                console.log(movieData);
+                console.log("-----------------------");
+                console.log("Title: " + movieData.Title);
+                console.log("Year: " + movieData.Year);
+                console.log("IMDB Rating: " + movieData.Ratings[0].Value);
+                console.log("Rotten Tomatoes Rating: " + movieData.Ratings[1].Value);
+                console.log("Country: " + movieData.Country);
+                console.log("Language: " + movieData.Language);
+                console.log("Plot: " + movieData.Plot);
+                console.log("Actors: " + movieData.Actors);
+            }
+        });
+    }
 
 } else if (command === 'do-what-it-says') {
+    fs.readFile(file, "utf-8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        } else {
 
-    // do-what-it-says logic
+            let randomFileArr = data.split(",");
+            let songName = randomFileArr[1];
+            let spotifyClient = new Spotify(spotifyKeys);
+
+            spotifyClient.search({
+                type: 'track',
+                query: songName,
+                limit: 1,
+            }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+
+                let trackData = data.tracks.items[0];
+
+                console.log(trackData);
+                console.log("-----------------------");
+                console.log("Artist(s): " + trackData.artists[0].name);
+                console.log("Title: " + trackData.name);
+                console.log("Preview Link: " + trackData.preview_url);
+                console.log("Album: " + trackData.album.name);
+            });
+        }
+    })
 
 } else {
     console.log('Command not recognized.');
