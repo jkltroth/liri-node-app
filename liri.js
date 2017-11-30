544059358d69a77303b0191c29aaede10c37f227
James Roth <jkltroth@gmail.com>
2017-11-29 23:43:47 -0500

    Refactored application so all major sections of code are in functions. Application is MVP.

--- liri.js
+++ liri.js
@@ -1,15 +1,18 @@
-const request = require("request");
+// Initializing all of my npm's, API keys, etc.
 const Twitter = require("twitter");
 const Spotify = require("node-spotify-api");
+const request = require("request");
 const fs = require('fs');
-
 const twitterKeys = require("./twitterKeys.js");
 const spotifyKeys = require("./spotifyKeys.js");
 const file = "random.txt";
 
-const command = process.argv[2].toLowerCase();
+// Capturing the users command/searchCriteria input and assinging to variables
+let command = process.argv[2];
+let searchCriteria = process.argv[3];
 
-if (command === 'my-tweets') {
+// A function to run the my-tweets command
+const myTweets = function () {
 
     let twitterClient = new Twitter(twitterKeys);
 
@@ -21,39 +24,40 @@ if (command === 'my-tweets') {
 
             tweets.forEach(function (element) {
                 console.log("-----------------------");
-                console.log("Tweet: " + element.text);
                 console.log("Created At: " + element.created_at);
+                console.log("Tweet: " + element.text);
             });
         }
     });
+}
 
-} else if (command === 'spotify-this-song') {
+// A function to run the spotify-this-song command
+const spotifyThisSong = function () {
 
-    let songName = process.argv[3];
     let spotifyClient = new Spotify(spotifyKeys);
 
-    if (!songName) {
+    if (!searchCriteria) {
 
         spotifyClient
             .request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
             .then(function (data) {
 
                 console.log("-----------------------");
-                console.log("Artist(s): " + data.artists[0].name);
                 console.log("Title: " + data.name);
-                console.log("Preview Link: " + data.preview_url);
+                console.log("Artist(s): " + data.artists[0].name);
                 console.log("Album: " + data.album.name);
+                console.log("Preview Link: " + data.preview_url);
 
             })
             .catch(function (err) {
                 console.error('Error occurred: ' + err);
             });
 
-    } else if (songName) {
+    } else if (searchCriteria) {
 
         spotifyClient.search({
             type: 'track',
-            query: songName,
+            query: searchCriteria,
             limit: 1,
         }, function (err, data) {
             if (err) {
@@ -61,23 +65,32 @@ if (command === 'my-tweets') {
             }
 
             let trackData = data.tracks.items[0];
+            let artistsArray = [];
 
-            console.log(trackData);
+            trackData.artists.forEach(function (element) {
+                artistsArray.push(element.name);
+            });
+
             console.log("-----------------------");
-            console.log("Artist(s): " + trackData.artists[0].name);
             console.log("Title: " + trackData.name);
-            console.log("Preview Link: " + trackData.preview_url);
+            console.log("Artist(s): " + artistsArray.join(", "));
             console.log("Album: " + trackData.album.name);
+
+            if (!trackData.preview_url) {
+                console.log("No preview link available.");
+            } else if (trackData.preview_url) {
+                console.log("Preview Link: " + trackData.preview_url);
+            }
         });
     }
+}
 
-} else if (command === 'movie-this') {
+// A function to run the movie-this command
+const movieThis = function () {
 
-    let movieName = process.argv[3];
+    if (!searchCriteria) {
 
-    if (!movieName) {
-
-        // Then run a request to the OMDB API with the movie specified
+        // Run a request to the OMDB API for Mr. Nobody
         request("http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=40e9cece", function (error, response, body) {
 
             // If the request is successful (i.e. if the response status code is 200)
@@ -85,9 +98,7 @@ if (command === 'my-tweets') {
 
                 let movieData = JSON.parse(body)
 
-                // Parse the body of the site and recover just the imdbRating
-                console.log("-----------------------");
-                console.log(movieData);
+                // Parse the body of the site and console.log data
                 console.log("-----------------------");
                 console.log("Title: " + movieData.Title);
                 console.log("Year: " + movieData.Year);
@@ -100,19 +111,17 @@ if (command === 'my-tweets') {
             }
         });
 
-    } else if (movieName) {
+    } else if (searchCriteria) {
 
-        // Then run a request to the OMDB API with the movie specified
-        request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece", function (error, response, body) {
+        // Run a request to the OMDB API for searchCriteria
+        request("http://www.omdbapi.com/?t=" + searchCriteria + "&y=&plot=short&apikey=40e9cece", function (error, response, body) {
 
             // If the request is successful (i.e. if the response status code is 200)
             if (!error && response.statusCode === 200) {
 
                 let movieData = JSON.parse(body)
 
-                // Parse the body of the site and recover just the imdbRating
-                console.log("-----------------------");
-                console.log(movieData);
+                // Parse the body of the site and console.log data
                 console.log("-----------------------");
                 console.log("Title: " + movieData.Title);
                 console.log("Year: " + movieData.Year);
@@ -125,38 +134,56 @@ if (command === 'my-tweets') {
             }
         });
     }
+}
 
-} else if (command === 'do-what-it-says') {
-    fs.readFile(file, "utf-8", function (error, data) {
+// A function to run the do-what-it-says command
+const doWhatItSays = function () {
+
+    fs.readFile(file, 'utf-8', function (error, data) {
         if (error) {
             return console.log(error);
         } else {
 
-            let randomFileArr = data.split(",");
-            let songName = randomFileArr[1];
-            let spotifyClient = new Spotify(spotifyKeys);
+            let randomFileArr = data.split(',');
+            command = randomFileArr[0].toLowerCase();
+            searchCriteria = randomFileArr[1];
 
-            spotifyClient.search({
-                type: 'track',
-                query: songName,
-                limit: 1,
-            }, function (err, data) {
-                if (err) {
-                    return console.log('Error occurred: ' + err);
-                }
-
-                let trackData = data.tracks.items[0];
-
-                console.log(trackData);
-                console.log("-----------------------");
-                console.log("Artist(s): " + trackData.artists[0].name);
-                console.log("Title: " + trackData.name);
-                console.log("Preview Link: " + trackData.preview_url);
-                console.log("Album: " + trackData.album.name);
-            });
+            if (command === 'my-tweets') {
+                myTweets();
+            } else if (command === 'spotify-this-song') {
+                spotifyThisSong();
+            } else if (command === 'movie-this') {
+                movieThis();
+            } else {
+                console.log('-----------------------');
+                console.log('Command not recognized.');
+            }
         }
-    })
+    });
+}
 
+if (!command) {
+    console.log('-----------------------');
+    console.log('Please enter a command.')
+    console.log('Command options are: ');
+    console.log('> my-tweets');
+    console.log('> spotify-this-song');
+    console.log('> movie-this');
+    console.log('> do-what-it-says');
+} else if (command.toLowerCase() === 'my-tweets') {
+    myTweets();
+} else if (command.toLowerCase() === 'spotify-this-song') {
+    spotifyThisSong();
+} else if (command.toLowerCase() === 'movie-this') {
+    movieThis();
+} else if (command.toLowerCase() === 'do-what-it-says') {
+    doWhatItSays();
 } else {
+    console.log('-----------------------');
     console.log('Command not recognized.');
+    console.log('Command options are: ');
+    console.log('> my-tweets');
+    console.log('> spotify-this-song');
+    console.log('> movie-this');
+    console.log('> do-what-it-says');
 }
